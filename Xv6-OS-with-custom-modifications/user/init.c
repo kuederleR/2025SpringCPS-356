4bd87af934ca0f2d806d9452b5b065b4df84fa06
Ryan Kuederle <ryankuederle@icloud.com>
2025-03-06 00:00:26 -0500

    Added queue init

--- Xv6-OS-with-custom-modifications/user/init.c
+++ Xv6-OS-with-custom-modifications/user/init.c
@@ -27,6 +27,14 @@ int main(void) {
   }
   close(hellofd);
 
+  int queuefd = open("dev/queue", O_RDWR);
+  if(queuefd < 0){
+    mknod("dev/queue", DQUEUE, 1); // 7 is major number, 1 is minor number 
+    queuefd = open("dev/queue", O_RDWR);
+  }
+  close(queuefd);
+
+
   for (;;) {
     printf(1, "init: starting sh\n");
     pid = fork();
