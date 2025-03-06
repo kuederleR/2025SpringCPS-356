3f084a7770af2ffa7778e4135a38bf3b09a3e749
Ryan Kuederle <ryankuederle@icloud.com>
2025-03-05 23:18:28 -0500

    updated queueinit def

--- Xv6-OS-with-custom-modifications/defs.h
+++ Xv6-OS-with-custom-modifications/defs.h
@@ -29,7 +29,7 @@ void panic(char *) __attribute__((noreturn));
 // dhello.c
 void helloinit(void);
 //dqueue.c
-void queue_init(void);
+void queueinit(void);
 
 // exec.c
 int exec(char *, char **);
