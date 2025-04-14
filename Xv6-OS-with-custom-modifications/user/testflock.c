7d565b8798ea580c33cf41bd34e37037b933bef4
Ryan Kuederle <ryankuederle@icloud.com>
2025-04-14 13:13:04 -0400

    Remove second lock to avoid deadlock.

--- Xv6-OS-with-custom-modifications/user/testflock.c
+++ Xv6-OS-with-custom-modifications/user/testflock.c
@@ -9,7 +9,7 @@ main(int argc, char *argv[])
     int lockFd = open("consoleLock", O_CREATE|O_RDWR);
     printf(1, "lockFd is %d\n", lockFd);
     flock(lockFd);
-    flock(lockFd);
+    // flock(lockFd);
     funlock(lockFd);
     exit();
 }
