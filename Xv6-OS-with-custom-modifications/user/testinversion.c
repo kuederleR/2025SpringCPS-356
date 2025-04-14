b7e30f13e48f92224807cd2df2f0331ada90cdb1
Ryan Kuederle <ryankuederle@icloud.com>
2025-04-14 01:30:27 -0400

    Removed stuff to do compilation test.

--- Xv6-OS-with-custom-modifications/user/testinversion.c
+++ Xv6-OS-with-custom-modifications/user/testinversion.c
@@ -8,71 +8,70 @@
 int flock(int fd, int operation);
 int funlock(int fd);
 
-int lockfd;
+// int lockfd;
 
-void low_process();
-void medium_process();
-void high_process();
+// void low_process();
+// void medium_process();
+// void high_process();
 
 int
-main(void)
-{
-  printf(1, "[MAIN] Priority inversion test.\n");
+main(void) {
+//   printf(1, "[MAIN] Priority inversion test.\n");
 
-  // create the lock file
-  lockfd = open("lockfile", O_CREATE | O_RDWR);
-  if (lockfd < 0) {
-    printf(2, "[ERROR] open lockfile failed.\n");
-    exit();
-  }
+//   // create the lock file
+//   lockfd = open("lockfile", O_CREATE | O_RDWR);
+//   if (lockfd < 0) {
+//     printf(2, "[ERROR] open lockfile failed.\n");
+//     exit();
+//   }
 
-  int pid_low = fork();
-  if (pid_low == 0) {
-    nice(getpid(), 1); // low priority
-    low_process();
-    exit();
-  }
+//   int pid_low = fork();
+//   if (pid_low == 0) {
+//     nice(getpid(), 1); // low priority
+//     low_process();
+//     exit();
+//   }
 
-  sleep(5);
+//   sleep(5);
 
-  int pid_high = fork();
-  if (pid_high == 0) {
-    nice(getpid(), 9); // high priority
-    high_process();
-    exit();
-  }
+//   int pid_high = fork();
+//   if (pid_high == 0) {
+//     nice(getpid(), 9); // high priority
+//     high_process();
+//     exit();
+//   }
 
-  sleep(5);
+//   sleep(5);
 
-  int pid_med = fork();
-  if (pid_med == 0) {
-    nice(getpid(), 5); // medium priority
-    medium_process();
-    exit();
-  }
+//   int pid_med = fork();
+//   if (pid_med == 0) {
+//     nice(getpid(), 5); // medium priority
+//     medium_process();
+//     exit();
+//   }
 
-  for (int i = 0; i < 3; i++)
-    wait();
+//   for (int i = 0; i < 3; i++)
+//     wait();
 
   printf(1, "[DONE]\n");
   exit();
 }
 
-void low_process() {
-  flock(lockfd, 1);
-  for (int i = 0; i < 20; i++) sleep(5);
-  funlock(lockfd);
-}
+// void low_process() {
+//   flock(lockfd, 1);
+//   for (int i = 0; i < 20; i++) sleep(5);
+//   funlock(lockfd);
+// }
 
-void high_process() {
-  flock(lockfd, 1);
-  funlock(lockfd);
-}
+// void high_process() {
+//   flock(lockfd, 1);
+//   funlock(lockfd);
+// }
 
-void medium_process() {
-  for (int i = 0; i < 50; i++) {
-    for (int j = 0; j < 100000; j++) {
-      asm volatile("");
-    }
-  }
-}
+// void medium_process() {
+//   for (int i = 0; i < 50; i++) {
+//     for (int j = 0; j < 100000; j++) {
+//       asm volatile("");
+//     }
+//   }
+// }
