0ed57bf44382bf5fc448827ec58c13abfb45bdde
Ryan Kuederle <ryankuederle@icloud.com>
2025-04-14 00:26:12 -0400

    Added a test for nice (temp)

---
+++ Xv6-OS-with-custom-modifications/user/nicetest.c
@@ -0,0 +1,24 @@
+// user/nicetest.c
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+  if(argc != 3){
+    fprintf(2, "Usage: nicetest <pid> <priority>\n");
+    exit(1);
+  }
+
+  int pid = atoi(argv[1]);
+  int prio = atoi(argv[2]);
+
+  int r = nice(pid, prio);
+  if(r < 0)
+    fprintf(2, "nice: failed to set priority for pid %d\n", pid);
+  else
+    printf("nice: priority of pid %d set to %d\n", pid, prio);
+
+  exit(0);
+}
